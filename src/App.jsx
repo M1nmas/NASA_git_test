0710da413c3a0fdb49f1c159d29b0ed27491ab87
M1nmas <raihanalsafa332@gmail.com>
2025-10-02 23:54:32 +0530

    all

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Para from './Para.jsx';
+
+function App() {
+  return (
+    <>
+      <Para />
+    </>
+  );
+}
+
+export default App
---
+++ src/Para.jsx
@@ -0,0 +1,9 @@
+export default function Para() {
+  return (
+    <>
+    <p>hello brooo</p>
+    <p>hello brooo</p>
+    <p>hello brooo</p>
+    </>
+  );
+}
